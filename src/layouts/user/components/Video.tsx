c9fc18863d9ddafb062602617f88c86001368971
vietphan565 <phantuanviet0706@gmail.com>
2025-11-18 14:33:42 +0700

    Update video object

--- src/layouts/user/components/Video.tsx
+++ src/layouts/user/components/Video.tsx
@@ -10,7 +10,7 @@ export const Video = ({ src, title = "", children }: VideoProps) => {
 	return (
 		<div className="relative w-full h-[75vh] overflow-hidden">
 			{children}
-			<video className="w-full h-full object-cover" autoPlay loop muted playsInline>
+			<video className="w-screen h-[80vh] object-cover" autoPlay loop muted playsInline>
 				<source src={src} type="video/mp4" />
 				Trình duyệt của bạn không hỗ trợ video.
 			</video>
